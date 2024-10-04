30f104225166cd8fdb7ff0351bb2ae19afa0c1ec
Dibakar Dey <110836157+dibakar63@users.noreply.github.com>
2024-10-04 13:09:06 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 const connectDb=async()=>{
     try {
-        await mongoose.connect('mongodb+srv://dibakardey63:JAwnTmfua0tOGBpt@recipes.rglhoml.mongodb.net/recipe_mern')
+        await mongoose.connect('mongodb+srv://dibakardey63:20Rx2Mx69vSKzNaZ@recipes.rglhoml.mongodb.net/recipe_mern')
         console.log("mongodb connected")
     } catch (error) {
         console.log(error)
